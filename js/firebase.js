8015a54e195a00b6a474f29be893f1f6114462ac
Im-your-hari <baluoger91@gmail.com>
2022-05-22 10:38:55 +0530

    Firebase file added

---
+++ js/firebase.js
@@ -0,0 +1,36 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDQLVAIOFBvZTTk_TdwrIfbubGDeDYXeUU",
+    authDomain: "technaura-contact.firebaseapp.com",
+    projectId: "technaura-contact",
+    storageBucket: "technaura-contact.appspot.com",
+    messagingSenderId: "951215708702",
+    appId: "1:951215708702:web:8b464288bb4d3945d0ec0f",
+    measurementId: "G-MHXHNY4PK7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Get reference of database
+const db = getDatabase(app)
+
+document.getElementById("frmContact").addEventListener('submit',function(e) {
+    e.preventDefault();
+    set(ref(db,'users/'+Math.random().toString(36).slice(2,7)),{
+        name : document.getElementById('contact-name').value,
+        email :  document.getElementById('contact-email').value,
+        subject : document.getElementById('contact-subject').value,
+        message : document.getElementById('contact-message').value
+
+    });
+    alert("Succesfully Submitted..!")
+    document.getElementById('frmContact').reset()
+});
