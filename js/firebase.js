b6c18a73e96485cfa7b51f46059ca4000142ba07
Im-your-hari <baluoger91@gmail.com>
2022-05-22 15:30:53 +0530

    firebase.js changed

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,4 @@
-
+/*
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js"
@@ -11,6 +11,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDQLVAIOFBvZTTk_TdwrIfbubGDeDYXeUU",
     authDomain: "technaura-contact.firebaseapp.com",
     projectId: "technaura-contact",
+    databaseURL:"https://technaura-contact-default-rtdb.firebaseio.com",
     storageBucket: "technaura-contact.appspot.com",
     messagingSenderId: "951215708702",
     appId: "1:951215708702:web:8b464288bb4d3945d0ec0f",
@@ -37,5 +38,41 @@ document.getElementById("frmContact").addEventListener('submit',function(e) {
     alert("Succesfully Submitted..!")
     document.getElementById('frmContact').reset()
 });
+*/
+const firebaseConfig = {
+    apiKey: "AIzaSyDQLVAIOFBvZTTk_TdwrIfbubGDeDYXeUU",
+    authDomain: "technaura-contact.firebaseapp.com",
+    projectId: "technaura-contact",
+    databaseURL:"https://technaura-contact-default-rtdb.firebaseio.com",
+    storageBucket: "technaura-contact.appspot.com",
+    messagingSenderId: "951215708702",
+    appId: "1:951215708702:web:8b464288bb4d3945d0ec0f",
+    measurementId: "G-MHXHNY4PK7"
+};
+firebase.initializeApp(firebaseConfig);
+//Ref initialization
+var msgRef = firebase.database().ref('messages');
 
-//document.getElementById('frmContact')
+document.getElementById('frmContact').addEventListener('submit',submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+    var name = document.getElementById('contact-name').value
+    var email = document.getElementById('contact-email').value
+    var subject = document.getElementById('contact-subject').value
+    var message = document.getElementById('contact-message').value
+
+    console.log(name,email,subject,message)
+    saveData(name,email,subject,message)
+}
+//Save data
+function saveData(name, email, subject, message) {
+    var newMsgRef = msgRef.push()
+    newMsgRef.set({
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    });
+}
